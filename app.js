c420121bc3c1d6612870549cf11ef5144a404825
Jalol <jalolkurbanov11@gmail.com>
2023-09-12 21:03:27 +0500

    s

---
+++ app.js
@@ -0,0 +1,25 @@
+var myName = prompt('Как вас зовут?');
+
+console.log('Меня зовут '+myName);
+
+var myAge = +prompt('Сколько вам лет?');
+
+console.log('Мне '+myAge+' лет/года/год');
+
+
+var problem1 = +prompt('123 + 68 = ?');
+console.log('Ответ: 123 + 68 = 191  Ваш ответ: ' + problem1);
+
+var problem2 = +prompt('187 - 23 = ?');
+console.log('Ответ: 187 - 23 = 164  Ваш ответ: ' + problem2);
+
+var problem3 = +prompt('12 * 8 = ?');
+console.log('Ответ: 12 * 8 = 96  Ваш ответ: ' + problem3);
+
+var problem4 = +prompt('360 / 45 = ?');
+console.log('Ответ: 360 / 45 = 8  Ваш ответ: ' + problem4);
+
+var problem5 = +prompt('1095 % 152 = ?');
+console.log('Ответ: 1095 % 152 = 31  Ваш ответ: ' + problem5);
+
+alert('Посмотрите в консоль чтобы посмотреть ответы');
